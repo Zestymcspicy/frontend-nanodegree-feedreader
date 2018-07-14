3e306117b11c99791ba6deaf3d6d280e62dc0367
Harold Beckerman <halbeckerman@gmail.com>
2018-07-13 21:00:56 -0500

    built a gulp watcher

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('default', function(){
+  gulp.watch('js/*.js').on('change', browserSync.reload);
+  gulp.watch('index.html').on('change', browserSync.reload);
+  gulp.watch('css/*.css').on('change', browserSync.reload);
+  browserSync.init({
+    server:  {
+        baseDir: "./",
+      }
+  });
+  });
